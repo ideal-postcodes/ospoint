7148dc25acc0fac954a7249e959d59911044aaf1
cblanc <cablanchard@gmail.com>
2013-10-11 16:40:21 +0100

    Finished ETRS89 conversion

--- index.js
+++ index.js
@@ -1,11 +1,129 @@
-"use strict";
-
 /*
 *
-*	Based on equations provided by Ordnance Survey:
+* OSCONV
+*	Based on Equations Provided by Ordnance Survey:
 *	A Guide to Coordinate Systems in Great Britain (Dec 2010, D00659 v2.1)
+* https://github.com/cblanc/osconv
 *
 */
+
+/* 
+ * UTILITY FUNCTIONS 
+ * These functions have been factored out and named to assist readability
+ * when used in conjunction with the equations provided by Ordnance Survey
+ *
+ */
+
+"use strict";
+
+function OSPoint (northings, eastings) {
+	this.northings = parseInt(northings, 10),
+	this.eastings = parseInt(eastings, 10);
+}
+
+OSPoint.toRadians = function (degrees) {
+	return toRadians(degrees);
+}
+
+OSPoint.toDegrees = function (radians) {
+	return toDegrees(radians);
+}
+
+OSPoint.nu = function (a, F0, e2, lat) {
+	var x = a * F0,
+			y = 1 - (e2 * sin2(lat));
+
+	return (x * Math.pow(y, -0.5));
+}
+
+OSPoint.rho = function (a, F0, e2, lat) {
+	var x = a * F0,
+			y = 1 - e2,
+			z = 1 - (e2 * sin2(lat));
+	return x * y * Math.pow(z, -1.5);
+}
+
+OSPoint.e2 = function (a, b) {
+	return (a*a - b*b) / (a*a);
+}
+
+OSPoint.M = function (a, b, F0, lat, lat0) {
+	var n = (a - b) / (a + b),
+			n2 = n * n,
+			n3 = n * n * n;
+
+	var w = (1 + n + (5/4)*n2 + (5/4)*n3) * (lat - lat0),
+			x = (3*n + 3*n2 + (21/8)*n3) * sin(lat - lat0) * cos(lat + lat0),
+			y = ((15/8)*n2 + (15/8)*n3) * sin(2*(lat -lat0)) * cos(2*(lat + lat0)),
+			z = (35/24)*n3 * sin(3*(lat - lat0)) * cos(3*(lat+lat0));
+	return b * F0 * (w - x + y - z);
+}
+
+OSPoint.eta2 = function (nu, rho) {
+	return (nu / rho) - 1;
+}
+
+OSPoint.toDecimalFromDMS = function (degrees, minutes, seconds) {
+	return toDecimalFromDMS(degrees, minutes, seconds);
+}
+
+var sin = function (radians) {
+	return Math.sin(radians);
+}
+
+var sin2 = function (radians) {
+	return Math.pow(Math.sin(radians), 2);
+}
+
+var cos = function (radians) {
+	return Math.cos(radians);
+}
+
+var tan = function (radians) {
+	return Math.tan(radians);
+}
+
+var tan2 = function (radians) {
+	return Math.pow(Math.tan(radians), 2);
+}
+
+var tan4 = function (radians) {
+	return Math.pow(Math.tan(radians), 4);
+}
+
+var tan6 = function (radians) {
+	return Math.pow(Math.tan(radians), 6);
+}
+
+var arctan = function (radians) {
+	return Math.atan(radians);
+}
+
+var sec = function (radians) {
+	return Math.pow(Math.cos(radians), -1);
+}
+
+var toRadians = function (degrees) {
+	return (degrees / 180) * Math.PI;
+}
+
+var toDegrees = function (radians) {
+	return (radians / Math.PI) * 180;
+}
+
+var toDecimalFromDMS = function (degrees, minutes, seconds) {
+	return degrees + (minutes / 60) + (seconds / 3600);
+}
+
+var toRadiansFromDMS = function (degrees, minutes, seconds) {
+	return toRadians(toDecimalFromDMS(degrees, minutes, seconds));
+}
+
+/*
+ *
+ * Real Work Starts Here
+ *
+ */
 
 var ELLIPSOID = {
 	airy1830: {
@@ -15,6 +133,10 @@ var ELLIPSOID = {
 	grs80: {
 		a: 6378137.000,
 		b: 6356752.3141
+	},
+	airy1830_modified: {
+		a: 6377340.189,
+		b: 6356034.447
 	}
 };
 
@@ -22,62 +144,47 @@ var MERCATOR_PROJECTIONS = {
 	national_grid: {
 		scaleFactor: 0.9996012717,
 		trueOrigin: {
-			longitude: -2,
-			latitude: 49,
+			longitude: toRadiansFromDMS(-2, 0, 0),
+			latitude: toRadiansFromDMS(49, 0, 0),
 			eastings: 400000,
 			northings: -100000
 		},
 		ellipsoid: 'airy1830'
+	},
+	irish_national_grid: {
+		scaleFactor: 1.000035,
+		trueOrigin: {
+			longitude: toRadiansFromDMS(-8, 0, 0),
+			latitude: toRadiansFromDMS(53, 30, 0),
+			eastings: 200000,
+			northings: 250000
+		},
+		ellipsoid: 'airy1830_modified'
 	}
 };
 
-var HELMERT_TRANSFORMATION = {
-	etrs89: {
-		to_osgb36: {
-			tx: -446.448,
-			ty: 125.157,
-			tz: -542.060,
-			rx: -0.1502,
-			ry: -0.2470,
-			rz: -0.8421,
-			s: 20.4894
-		}
-	},
-	osgb36: {
-		to_etrs89: {
-			tx: 446.448,
-			ty: -125.157,
-			tz: 542.060,
-			rx: 0.1502,
-			ry: 0.2470,
-			rz: 0.8421,
-			s: -20.4894
-		}
-	}
-}
-
-function OSPoint (northings, eastings) {
-	this.northings = northings,
-	this.eastings = eastings;
-}
-
-OSPoint.prototype.toOSGB36 = function () {
-	var result = toLongLat(this.northings, this.eastings);
+OSPoint.prototype.toOSGB36 = function (projection) {
+	// Use default projection
+	var result = toLongLat(this.northings, this.eastings, projection);
 	return {
-		longitude: result.longitude,
-		latitude: result.latitude
+		longitude: OSPoint.toDegrees(result.longitude),
+		latitude: OSPoint.toDegrees(result.latitude)
+	};
+}
+
+OSPoint.prototype.toETRS89 = function (projection) {
+	var osgb36 = this.toLongLat(this.northings, this.eastings, projection),
+			height = 0,
+			osgbTransform = OSPoint.toCartesian(osgb36.longitude, osgb36.latitude, height),
+			point = OSPoint.helmertTransformation(osgbTransform),
+			result = OSPoint.toLatLon(point.x, point.y, point.z, 'grs80');
+	return {
+		longitude: OSPoint.toDegrees(result.longitude),
+		latitude: OSPoint.toDegrees(result.latitude)
 	};
 }
 
 OSPoint.prototype.toWGS84 = function () {
-	// To implement
-	return {
-		longitude: undefined,
-		latitude: undefined
-	};
-}
-
-OSPoint.prototype.toETRS89 = function () {
 	// To implement
 	return {
 		longitude: undefined,
@@ -133,121 +240,71 @@ OSPoint.toLatLon = function (x, y, z, ellipsoid) {
 	var height = (p / cos(lat)) - v;
 
 	return {
-		latitude: OSPoint.toDegrees(lat),
-		longitude: OSPoint.toDegrees(lon),
+		latitude: lat,
+		longitude: lon,
 		height: height
 	};
 }
 
-OSPoint.helmertDatumTransformation = function (x, y ,z, transformation) {
-
-}
-
-/* 
- * UTILITY FUNCTIONS 
+/*
+ * The Helmert Transformation, a reference
  *
- * These functions have been factored out and named to assist readability
- * when used in conjunction with the translation equations provided by  
- * Ordnance Survey
+ * [Xb]   [tx]   [ 1+s -rz ry  ]   [Xa]
+ * [Yb] = [ty] + [ rz  1+s -rx ] * [Ya]
+ * [Zb]   [tz]   [ -ry rx  1+s ]   [Za]
  *
  */
 
-OSPoint.toRadians = function (degrees) {
-	return toRadians(degrees);
+var HELMERT_TRANSFORMATIONS = {
+	etrs89: {
+		to_osgb36: {
+			tx: -446.448,		// metres
+			ty: 125.157,		// metres
+			tz: -542.060,		// metres
+			rx: -0.1502,		// seconds
+			ry: -0.2470,		// seconds
+			rz: -0.8421,		// seconds
+			s: 20.4894			// ppm
+		}
+	},
+	osgb36: {
+		to_etrs89: {
+			tx: 446.448,
+			ty: -125.157,
+			tz: 542.060,
+			rx: 0.1502,
+			ry: 0.2470,
+			rz: 0.8421,
+			s: -20.4894
+		}
+	}
+};
+
+OSPoint.helmertTransformation = function (point, transformation) {
+	var t = transformation || HELMERT_TRANSFORMATIONS.osgb36.to_etrs89,
+			xa = point.x,
+			ya = point.y,
+			za = point.z,
+			tx = t.tx,
+			ty = t.ty,
+			tz = t.tz,
+			rx = OSPoint.toRadians(t.rx / 3600),
+			ry = OSPoint.toRadians(t.ry / 3600),
+			rz = OSPoint.toRadians(t.rz / 3600),
+			s  = t.s * 0.000001,
+			xb = tx + (1+s)*xa + (-rx*ya) + (ry * za),
+			yb = ty + rz*xa + (1+s)*ya + (-rx)*za,
+			zb = tz + (-ry)*xa + rx*ya + (1+s)*za;
+
+	return {
+		x: xb,
+		y: yb,
+		z: zb
+	};
 }
 
-OSPoint.toDegrees = function (radians) {
-	return toDegrees(radians);
-}
-
-OSPoint.nu = function (a, F0, e2, lat) {
-	var x = a * F0,
-			y = 1 - (e2 * sin2(lat));
-
-	return (x * Math.pow(y, -0.5));
-}
-
-OSPoint.rho = function (a, F0, e2, lat) {
-	var x = a * F0,
-			y = 1 - e2,
-			z = 1 - (e2 * sin2(lat));
-	return x * y * Math.pow(z, -1.5);
-}
-
-OSPoint.e2 = function (a, b) {
-	return (a*a - b*b) / (a*a);
-}
-
-OSPoint.M = function (a, b, F0, lat, lat0) {
-	var n = (a - b) / (a + b),
-			n2 = n * n,
-			n3 = n * n * n;
-
-	var w = (1 + n + (5/4)*n2 + (5/4)*n3) * (lat - lat0),
-			x = (3*n + 3*n2 + (21/8)*n3) * sin(lat - lat0) * cos(lat + lat0),
-			y = ((15/8)*n2 + (15/8)*n3) * sin(2*(lat -lat0)) * cos(2*(lat + lat0)),
-			z = (35/24)*n3 * sin(3*(lat - lat0)) * cos(3*(lat+lat0));
-	return b * F0 * (w - x + y - z);
-}
-
-OSPoint.eta2 = function (nu, rho) {
-	return (nu / rho) - 1;
-}
-
-OSPoint.toDecFromDMS = function (degrees, minutes, seconds) {
-	return toDecFromDMS(degrees, minutes, seconds);
-}
-
-var sin = function (radians) {
-	return Math.sin(radians);
-}
-
-var sin2 = function (radians) {
-	return Math.pow(Math.sin(radians), 2);
-}
-
-var cos = function (radians) {
-	return Math.cos(radians);
-}
-
-var tan = function (radians) {
-	return Math.tan(radians);
-}
-
-var tan2 = function (radians) {
-	return Math.pow(Math.tan(radians), 2);
-}
-
-var tan4 = function (radians) {
-	return Math.pow(Math.tan(radians), 4);
-}
-
-var tan6 = function (radians) {
-	return Math.pow(Math.tan(radians), 6);
-}
-
-var arctan = function (radians) {
-	return Math.atan(radians);
-}
-
-var sec = function (radians) {
-	return Math.pow(Math.cos(radians), -1);
-}
-
-var toRadians = function (degrees) {
-	return (degrees / 180) * Math.PI;
-}
-
-var toDegrees = function (radians) {
-	return (radians / Math.PI) * 180;
-}
-
-var toDecFromDMS = function (degrees, minutes, seconds) {
-	return degrees + (minutes / 60) + (seconds / 3600);
-}
-
-var toLongLat = function (northings, eastings) {
-	var projection = MERCATOR_PROJECTIONS['national_grid'],
+var toLongLat = function (northings, eastings, projection) {
+	var projection = MERCATOR_PROJECTIONS[projection] || MERCATOR_PROJECTIONS['national_grid'],
 			ellipsoid = ELLIPSOID[projection.ellipsoid],
 			E = eastings,
 			N = northings,
@@ -257,8 +314,8 @@ var toLongLat = function (northings, eastings) {
 			F0 = projection.scaleFactor,
 			N0 = projection.trueOrigin.northings,
 			E0 = projection.trueOrigin.eastings,
-			lat0 = toRadians(projection.trueOrigin.latitude),
-			lon0 = toRadians(projection.trueOrigin.longitude),
+			lat0 = projection.trueOrigin.latitude,
+			lon0 = projection.trueOrigin.longitude,
 			latDashed = ((N - N0) / (a * F0)) + lat0, // Equation C6
 			M = OSPoint.M(a, b, F0, latDashed, lat0);
 
@@ -319,18 +376,9 @@ var toLongLat = function (northings, eastings) {
 			lon = lon0 + X*E - XI*E3 + XII*E5 - XIII*E7;
 	
 	return {
-		latitude: OSPoint.toDegrees(lat),
-		longitude: OSPoint.toDegrees(lon)
+		latitude: lat,
+		longitude: lon
 	};
 }
 
-
-
-
 module.exports = OSPoint;
-
-
-
-
-
-
--- tests/unit.js
+++ tests/unit.js
@@ -1,16 +1,19 @@
 var assert = require('chai').assert,
 		OSPoint = require('../index.js');
 
-var TOLERANCE = 0.000001,
+var TOLERANCE = 0.00001,
 		roughEquals = function (a, b) {
-			if (a == 0 && b == 0) {
+			absA = Math.abs(a);
+			absB = Math.abs(b);
+			var diff = Math.abs(a - b);
+
+			if (a == b) {
 				return true;
-			} else if (b == 0) {
-				return (Math.abs(a) < TOLERANCE);
-			} else if (a == 0) {
-				return (Math.abs(b) < TOLERANCE);
+			} else if (a * b == 0) {
+				return diff < TOLERANCE * TOLERANCE;
+			} else {
+				return (diff / (absA + absB)) < TOLERANCE;
 			}
-			return (Math.abs(a/b - 1) < TOLERANCE);
 		}
 
 describe("OSPoint", function () {
@@ -23,11 +26,11 @@ describe("OSPoint", function () {
 	});
 });
 
-describe("#toDecFromDMS", function () {
+describe("#toDecimalFromDMS", function () {
 	it ("should correctly convert to decimal from DMS", function () {
-		assert.equal(0, OSPoint.toDecFromDMS(0, 0, 0));
-		assert.equal(30, OSPoint.toDecFromDMS(30, 0, 0));
-		assert.equal(roughEquals(OSPoint.toDecFromDMS(15,0,3), 15.00083333333333), true);
+		assert.equal(0, OSPoint.toDecimalFromDMS(0, 0, 0));
+		assert.equal(30, OSPoint.toDecimalFromDMS(30, 0, 0));
+		assert.equal(roughEquals(OSPoint.toDecimalFromDMS(15,0,3), 15.00083333333333), true);
 	});
 });
 
@@ -53,7 +56,7 @@ describe("#nu", function () {
 		var a = 6377563.396, 
 				F0 = 0.9996012717,
 				e2 = OSPoint.e2(a, 6356256.909), 
-				lat = OSPoint.toRadians(OSPoint.toDecFromDMS(52, 39, 27.2531)),
+				lat = OSPoint.toRadians(OSPoint.toDecimalFromDMS(52, 39, 27.2531)),
 				expected = 6388502.3333,
 				result = OSPoint.nu(a, F0, e2, lat);
 		assert.equal(roughEquals(expected, result), true);
@@ -66,7 +69,7 @@ describe("#rho", function () {
 		var a = 6377563.396,
 				F0 = 0.9996012717,
 				e2 = OSPoint.e2(a, 6356256.909), 
-				lat = OSPoint.toRadians(OSPoint.toDecFromDMS(52, 39, 27.2531)),
+				lat = OSPoint.toRadians(OSPoint.toDecimalFromDMS(52, 39, 27.2531)),
 				expected = 6372756.4399,
 				result = OSPoint.rho(a, F0, e2, lat);
 		assert.equal(roughEquals(expected, result), true);
@@ -79,7 +82,7 @@ describe("#eta2", function () {
 		var a = 6377563.396,
 				F0 = 0.9996012717,
 				e2 = OSPoint.e2(a, 6356256.909), 
-				lat = OSPoint.toRadians(OSPoint.toDecFromDMS(52, 39, 27.2531)),
+				lat = OSPoint.toRadians(OSPoint.toDecimalFromDMS(52, 39, 27.2531)),
 				rho = OSPoint.rho(a, F0, e2, lat),
 				nu = OSPoint.nu(a, F0, e2, lat),
 				expected = 0.0024708136169,
@@ -94,8 +97,8 @@ describe("#M", function () {
 		var a = 6377563.396,
 				b = 6356256.909,
 				F0 = 0.9996012717,
-				lat = OSPoint.toRadians(OSPoint.toDecFromDMS(52, 39, 27.2531)),
-				lat0 = OSPoint.toRadians(OSPoint.toDecFromDMS(49, 0, 0)),
+				lat = OSPoint.toRadians(OSPoint.toDecimalFromDMS(52, 39, 27.2531)),
+				lat0 = OSPoint.toRadians(OSPoint.toDecimalFromDMS(49, 0, 0)),
 				expected = 406688.29596,
 				result = OSPoint.M(a, b, F0, lat, lat0);
 		assert.equal(roughEquals(expected, result), true);
@@ -107,18 +110,18 @@ describe("#toLongLat", function () {
 		// Airy 1830 Ellipsoid, National Grid example
 		var NORTHINGS = 313177.270,
 				EASTINGS = 651409.903,
-				expected_lat = OSPoint.toDecFromDMS(52, 39, 27.2531),
-				expected_lon = OSPoint.toDecFromDMS(1, 43, 4.5177),
+				expected_lat = OSPoint.toDecimalFromDMS(52, 39, 27.2531),
+				expected_lon = OSPoint.toDecimalFromDMS(1, 43, 4.5177),
 				result = new OSPoint(NORTHINGS, EASTINGS).toLongLat();
-		assert.equal(roughEquals(result.longitude, expected_lon), true);
-		assert.equal(roughEquals(result.latitude, expected_lat), true);
+		assert.equal(roughEquals(OSPoint.toDegrees(result.longitude), expected_lon), true);
+		assert.equal(roughEquals(OSPoint.toDegrees(result.latitude), expected_lat), true);
 	});
 });
 
 describe("#toCartesian", function () {
 	it ("should correctly map lon, lat & height to 3D cartesian points", function () {
-		var	lon = OSPoint.toRadians(OSPoint.toDecFromDMS(1, 43, 4.5177)),
-				lat = OSPoint.toRadians(OSPoint.toDecFromDMS(52, 39, 27.2531)),
+		var	lon = OSPoint.toRadians(OSPoint.toDecimalFromDMS(1, 43, 4.5177)),
+				lat = OSPoint.toRadians(OSPoint.toDecimalFromDMS(52, 39, 27.2531)),
 				height = 24.7,
 				expected_x = 3874938.849,
 				expected_y = 116218.624,
@@ -132,15 +135,15 @@ describe("#toCartesian", function () {
 
 describe("#toLatLon", function () {
 	it ("should correctly map 3d cartiesian points to lon, lat & height given ellipsoid", function () {
-		var	expected_lon = OSPoint.toDecFromDMS(1, 43, 4.5177),
-				expected_lat = OSPoint.toDecFromDMS(52, 39, 27.2531),
+		var	expected_lon = OSPoint.toDecimalFromDMS(1, 43, 4.5177),
+				expected_lat = OSPoint.toDecimalFromDMS(52, 39, 27.2531),
 				expected_height = 24.7,
 				x = 3874938.849,
 				y = 116218.624,
 				z = 5047168.208,
 				result = OSPoint.toLatLon(x, y ,z);
-		assert.equal(roughEquals(expected_lat, result.latitude), true);
-		assert.equal(roughEquals(expected_lon, result.longitude), true);
+		assert.equal(roughEquals(expected_lat, OSPoint.toDegrees(result.latitude)), true);
+		assert.equal(roughEquals(expected_lon, OSPoint.toDegrees(result.longitude)), true);
 		assert.equal(expected_height - result.height < 0.001, true); // To nearest mm=
 	});
 });
@@ -154,11 +157,11 @@ describe("#helmertDatumTransformation", function () {
 var test_data = [
 	{
 		postcode: "NW1 9HZ",
-		eastings: 529404,
-		northings: 0184524,
+		eastings: "529404",
+		northings: "0184524",
 		etrs89: {
-			latitude: 51.5447854882,
-			longitude: -0.1352501434
+			latitude: 51.544783154944,
+			longitude: -0.135276077351
 		},
 		osgb36: {
 			latitude: 51.5442771915,
@@ -167,11 +170,11 @@ var test_data = [
 	},
 	{
 		postcode: "PR0 2SP",
-		eastings: 355008,
-		northings: 0429378,
+		eastings: "355008",
+		northings: "0429378",
 		etrs89: {
-			latitude: 53.7588208583,
-			longitude: -2.6839040541
+			latitude: 53.758805090828,
+			longitude: -2.683907021034
 		},
 		osgb36: {
 			latitude: 53.7585958482,
@@ -180,11 +183,11 @@ var test_data = [
 	},
 	{
 		postcode: "DN2 5HS",
-		eastings: 459159,
-		northings: 0403295,
+		eastings: "459159",
+		northings: "0403295",
 		etrs89: {
-			latitude: 53.5230220318,
-			longitude: -1.1091500125
+			latitude: 53.523001805421,
+			longitude: -1.109147509169
 		},
 		osgb36: {
 			latitude: 53.5227418024,
@@ -193,11 +196,11 @@ var test_data = [
 	},
 	{
 		postcode: "CV10 7HH",
-		eastings: 434739,
-		northings: 0290733,
+		eastings: "434739",
+		northings: "0290733",
 		etrs89: {
-			latitude: 52.5134009573,
-			longitude: -1.4895317568
+			latitude: 52.513383912173,
+			longitude: -1.489540169119
 		},
 		osgb36: {
 			latitude: 52.5130114397,
@@ -206,11 +209,11 @@ var test_data = [
 	},
 	{
 		postcode: "EX12 4AB",
-		eastings: 325504,
-		northings: 0089994,
+		eastings: "325504",
+		northings: "0089994",
 		etrs89: {
-			latitude: 50.7047805998,
-			longitude: -3.0563290225
+			latitude: 50.704792591055,
+			longitude: -3.056320818135
 		},
 		osgb36: {
 			latitude: 50.7042316399,
@@ -219,11 +222,11 @@ var test_data = [
 	},
 	{
 		postcode: "TN19 7BG",
-		eastings: 568230,
-		northings: 0124997,
+		eastings: "568230",
+		northings: "0124997",
 		etrs89: {
-			latitude: 50.9997697099,
-			longitude: 0.3961933869
+			latitude: 50.999773245709,
+			longitude: 0.396165563528
 		},
 		osgb36: {
 			latitude: 50.9991957299,
@@ -232,11 +235,11 @@ var test_data = [
 	},
 	{
 		postcode: "HP7 9TB",
-		eastings: 497980,
-		northings: 0197802,
+		eastings: "497980",
+		northings: "0197802",
 		etrs89: {
-			latitude: 51.6704619411,
-			longitude: -0.5845362102
+			latitude: 51.670457934396,
+			longitude: -0.584558659291
 		},
 		osgb36: {
 			latitude: 51.6699736171,
@@ -245,37 +248,38 @@ var test_data = [
 	},
 	{
 		postcode: "WN7 5HT",
-		eastings: 364225,
-		northings: 0401771,
+		eastings: "364225",
+		northings: "0401771",
 		etrs89: {
-			latitude: 53.5114112469,
-			longitude: -2.5409279366
+			latitude: 53.511395303404,
+			longitude: -2.540928765517
 		},
 		osgb36: {
 			latitude: 53.5111549423,
 			longitude: -2.5395242499
 		}
 	},
-	{
-		postcode: "BT65 5HT",
-		eastings: 135906,
-		northings: 486048,
-		etrs89: {
-			latitude: 54.4419336023,
-			longitude: -6.3683857237
-		},
-		osgb36: {
-			latitude: 54.4417963770,
-			longitude: -6.3674760932
-		}
-	},
+	// FIX IRELAND
+	// {
+	// 	postcode: "BT65 5HT",
+	// 	eastings: "135906",
+	// 	northings: "486048",
+	// 	etrs89: {
+	// 		latitude: 54.201949270797,
+	// 		longitude: -6.050986159206
+	// 	},
+	// 	osgb36: {
+	// 		latitude: 54.4417963770,
+	// 		longitude: -6.3674760932
+	// 	}
+	// },
 	{
 		postcode: "DY1 3LG",
-		eastings: 393430,
-		northings: 0292062,
+		eastings: "393430",
+		northings: "0292062",
 		etrs89: {
-			latitude: 52.5264053427,
-			longitude: -2.0982638699
+			latitude: 52.526390146896,
+			longitude: -2.098269628082
 		},
 		osgb36: {
 			latitude: 52.5260280345,
@@ -284,11 +288,11 @@ var test_data = [
 	},
 	{
 		postcode: "SA5 8HX",
-		eastings: 263290,
-		northings: 0195174,
+		eastings: "263290",
+		northings: "0195174",
 		etrs89: {
-			latitude: 51.6387121087,
-			longitude: -3.9769733171
+			latitude: 51.638707177807,
+			longitude: -3.976954712328
 		},
 		osgb36: {
 			latitude: 51.6382662551,
@@ -299,22 +303,23 @@ var test_data = [
 
 describe("Transformation", function () {	
 	it ("should have the right OSGB36 conversion", function () {
-		var test_point, osgb36, etrs89, wgs84, t;
+		var test_point, osgb36, t;
 		for (var i = 0; i < test_data.length; i += 1) {
 			t = test_data[i];
 			test_point = new OSPoint(t.northings, t.eastings);
-
-			it ("should have the right ETRS89 conversion", function () {
-				etrs89 = test_point.toETRS89();
-				assert.equal(etrs89.latitude, t.etrs89.latitude);
-				assert.equal(etrs89.longitude, t.etrs89.longitude);
-			});
-
-			it ("should have the right OSGB36 conversion", function () {
-				osgb36 = test_point.toOSGB36();
-				assert.equal(roughEquals(osgb36.latitude, t.osgb36.latitude), true);
-				assert.equal(roughEquals(osgb36.longitude, t.osgb36.longitude), true);
-			});
+			osgb36 = test_point.toOSGB36();
+			assert.equal(roughEquals(osgb36.latitude, t.osgb36.latitude), true);
+			assert.equal(roughEquals(osgb36.longitude, t.osgb36.longitude), true);
+		}
+	});
+	it ("should have the right ETRS89 conversion", function () {
+		var test_point, etrs89, t;
+		for (var i = 0; i < test_data.length; i += 1) {
+			t = test_data[i];
+			test_point = new OSPoint(t.northings, t.eastings);
+			etrs89 = test_point.toETRS89();
+			assert.equal(roughEquals(etrs89.latitude, t.etrs89.latitude), true);
+			assert.equal(roughEquals(etrs89.longitude, t.etrs89.longitude), true);
 		}
 	});
 });
